4045d3266a041af6b3233c2382aac8e032570d53
Christian Aquino <christian.a77@gmail.com>
2021-09-02 00:55:01 -0400

    added validations

--- src/components/new.js
+++ src/components/new.js
@@ -1,30 +1,71 @@
 import React, { useContext } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 import { FichaMedicaContext } from "../providers/FichaMedica";
+
+const schema = yup.object().shape({
+  rut: yup
+    .string()
+    .required("El campo rut es obligatorio")
+    .matches(/^(\d{1,3}(?:\.\d{3}){2}-[\dkK])$/, "Ingrese un rut válido"),
+  nombres: yup
+    .string()
+    .required("El campo nombres es obligatorio")
+    .matches(/^[a-zA-Z ]+$/, "El campo nombres debe contener sólo letras"),
+  apellidos: yup
+    .string()
+    .required("El campo apellidos es obligatorio")
+    .matches(/^[a-zA-Z ]+$/, "El campo apellidos debe contener sólo letras"),
+  direccion: yup.string().required("El campo dirección es obligatorio"),
+  ciudad: yup.string().required("El campo ciudad es obligatorio"),
+  telefono: yup
+    .string()
+    .required("El campo teléfono es obligatorio")
+    .matches(/[0-9]/, "El campo teléfono debe contener sólo múmeros")
+    .length(9, "El campo teléfono debe tener 9 caracteres"),
+  email: yup
+    .string()
+    .email("Ingrese un email válido")
+    .required("El campo email es obligatorio"),
+  fechaNacimiento: yup
+    .string()
+    .required("El campo fecha de nacimiento es obligatorio")
+    .matches(
+      /^([0][1-9]|[12][0-9]|3[01])(\/|-)([0][1-9]|[1][0-2])\2(\d{4})/g,
+      "Ingrese una feha válida"
+    ),
+});
 
 function New() {
   const [fichaMedicaList, setFichaMedicaList] = useContext(FichaMedicaContext);
 
   const fields = [
-    { name: "rut", label: "Rut", required: true },
-    { name: "nombres", label: "Nombres", required: true },
-    { name: "apellidos", label: "Apellidos", required: true },
-    { name: "direccion", label: "Direccion", required: true },
-    { name: "ciudad", label: "Ciudad", required: true },
-    { name: "telefono", label: "Telefono", required: true },
-    { name: "email", label: "Email", required: true },
-    { name: "fechaNacimiento", label: "Fecha de Nacimiento", required: true },
-    { name: "estadoCivil", label: "Estado Civil", required: true },
-    { name: "comentarios", label: "Comentarios", required: true },
+    { name: "rut", label: "Rut", placeHolder: "Ej: 7416161-8" },
+    { name: "nombres", label: "Nombres" },
+    { name: "apellidos", label: "Apellidos" },
+    { name: "direccion", label: "Direccion" },
+    { name: "ciudad", label: "Ciudad" },
+    { name: "telefono", label: "Telefono", placeHolder: "Ej: 971762643" },
+    { name: "email", label: "Email" },
+    {
+      name: "fechaNacimiento",
+      label: "Fecha de Nacimiento",
+      placeHolder: "Ej: dd/mm/aaaa",
+    },
+    { name: "estadoCivil", label: "Estado Civil" },
+    { name: "comentarios", label: "Comentarios" },
   ];
 
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const onSubmit = (data) =>
     setFichaMedicaList([...fichaMedicaList, { ...data }]);
@@ -32,26 +73,33 @@ function New() {
   return (
     <div className="container col-md-6">
       <div className="card">
-        <div class="card-header">Nuevo registro</div>
+        <div className="card-header">Nuevo registro</div>
         <div className="container py-2">
           <form onSubmit={handleSubmit(onSubmit)}>
-            {fields.map(({ name, label, required }) => (
-              <div class="mb-3">
-                <label for={name} class="form-label">
+            {fields.map(({ name, label, placeHolder }) => (
+              <div className="mb-3" key={Math.random()}>
+                <label htmlFor={name} className="form-label">
                   {label}
                 </label>
                 <input
-                  {...register(name, { required: required })}
-                  className="form-control"
+                  {...register(name)}
+                  className={`form-control form-control-sm ${
+                    errors[name] && "is-invalid"
+                  }`}
+                  placeholder={placeHolder || ""}
                 />
-                {errors[name] && <div class="form-text">Requerido</div>}
+                {errors[name] && (
+                  <div className="invalid-feedback">{errors[name].message}</div>
+                )}
               </div>
             ))}
 
             <input type="submit" className="btn btn-primary" value="Guardar" />
-            <button className="btn btn-secondary ms-2 me-2">Limpiar</button>
+            <button type="reset" className="btn btn-secondary ms-2 me-2">
+              Limpiar
+            </button>
             <Link to={"/"} className="btn btn-danger">
-              Cancelar
+              Volver al listado
             </Link>
           </form>
         </div>
