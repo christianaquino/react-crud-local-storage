9721d814b2ed884f2d8d99598600c70388ce6393
Christian Aquino <christian.a77@gmail.com>
2021-09-05 21:09:05 -0300

    save in localStorage

--- src/providers/FichaMedica.js
+++ src/providers/FichaMedica.js
@@ -1,12 +1,19 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import initialData from "../data/initialData";
 
 //create a context, with createContext api
 export const FichaMedicaContext = createContext();
 
 const FichaMedicaProvider = (props) => {
+  const data = localStorage.getItem("fichaMedicaList")
+    ? JSON.parse(localStorage.getItem("fichaMedicaList"))
+    : initialData;
   // this state will be shared with all components
-  const [fichaMedicaList, setFichaMedicaList] = useState(initialData);
+  const [fichaMedicaList, setFichaMedicaList] = useState(data);
+
+  useEffect(() => {
+    localStorage.setItem("fichaMedicaList", JSON.stringify(fichaMedicaList));
+  }, [fichaMedicaList]);
 
   return (
     // this is the provider providing state
